56fdc43f7471a3c8ce785ea3fe8747c1bed76647
Nathan Macnamara <nathan.macnamara@arrowecs.co.uk>
2013-08-02 14:40:23 +0100

    Initial Commit

---
+++ applications/application/core/MY_Router.php
@@ -0,0 +1,6 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/* load the MX_Router class */
+require SHARE."third_party/MX/Router.php";
+
+class MY_Router extends MX_Router {}
