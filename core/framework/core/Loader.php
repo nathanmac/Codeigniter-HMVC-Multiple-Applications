7bfed39274331ceeaa92d9ff436c4324fca284a7
Nathan Macnamara <nathan.macnamara@arrowecs.co.uk>
2013-08-05 09:34:10 +0100

    Support for events, plugin library - https://github.com/ericbarnes/CodeIgniter-Events

--- core/framework/core/Loader.php
+++ core/framework/core/Loader.php
@@ -124,8 +124,8 @@ class CI_Loader {
 	public function __construct()
 	{
 		$this->_ci_ob_level  = ob_get_level();
-		$this->_ci_library_paths = array(APPPATH, BASEPATH);
-		$this->_ci_helper_paths = array(APPPATH, BASEPATH);
+		$this->_ci_library_paths = array(APPPATH, BASEPATH, SHARE);
+		$this->_ci_helper_paths = array(APPPATH, BASEPATH, SHARE);
 		$this->_ci_model_paths = array(APPPATH);
 		$this->_ci_view_paths = array(APPPATH.'views/'	=> TRUE);
 
